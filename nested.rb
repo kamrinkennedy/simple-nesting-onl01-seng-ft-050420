6b357e8d84d6a0ff1f2c6a91bccd90c429b1db57
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-08 16:54:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,7 +65,7 @@ def adding_matz
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
+#     :languages => ["LISP", "C"]
 # }
 
 	programmer_hash = 
@@ -83,8 +83,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
     
+    programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["LISP", "C"]}
 end
 
 def changing_alan
@@ -105,7 +105,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
      
 end
 
